a51dcf01987bcaa856c2e108594c6382a4abdf9d
denisjul <mr.jdenis@gmail.com>
2016-06-13 20:57:18 +1200

    Update classe_pdf.py
    file update: 'Tableau' object replace by 'Table' Object contained in the new file 'html_to_ascii_table'
    The New Table object, is Juju's maid and has better results than the DashTable found on an other Git

--- classe_pdf.py
+++ classe_pdf.py
@@ -2,6 +2,8 @@
 
 """ contains classes """
 
+from html_to_ascii_table import Table
+
 
 class Article:
     """ Object containing all the paramaeters
@@ -18,7 +20,7 @@ class Article:
         txt += self.name + "\n"
         txt += "=" * 70 + "\n"
         for i in range(len(self.contents)):
-            if isinstance(self.contents[i], Tableau):
+            if isinstance(self.contents[i], Table):
                 txt += self.contents[i].represent + "\n\n"
             else:
                 txt += self.contents[i] + "\n\n"
@@ -78,90 +80,3 @@ class Code_de_lois:
                 if isinstance(self.contents[i], Article) is not True:
                     txt += self.in_contents(self.contents[i])
         return txt
-
-
-class Tableau():
-    """ Class maid to recover tables from a legifrance page """
-
-    def __init__(self, html):
-        self.html = html
-        self.tab = self._html_to_tab()
-        self.represent = self._get_represent()
-
-    def _html_to_tab(self):
-        """ tranform the html source code to a Tableau object"""
-        nb_lines = self.html.count('</tr>')
-        nb_columns = self.html.count('</th>')
-        self.html = self.html.replace('<p>', '')
-        self.html = self.html.replace('</p>', '')
-        self.html = self.html.replace('<br/>', '')
-        self.html = self.html.replace('<tr>', '')
-        self.html = self.html.replace('</tr>', '')
-        self.html = self.html.replace('<tbody>', '')
-        self.html = self.html.replace('</tbody>', '')
-        tab = []
-        i = 0
-        for x in range(nb_lines):
-            tab.append([])
-            for y in range(nb_columns):
-                if x == 0:
-                    start = '<th'
-                    end = '</th>'
-                else:
-                    start = '<td'
-                    end = '</td>'
-                data, i = self._get_something(start,
-                                              end, i, code_source=self.html)
-                data = data.replace("\n", "")
-                tab[x].append(data)
-        return tab
-
-    def _get_something(self, start, end, i, code_source=""):
-        """ Recover the text between 'start' and 'end' (as str),
-        'start' & 'end' excluded"""
-        length_start = len(start)
-        length_end = len(end)
-        buf_start = ""
-        buf_end = ""
-        text = ""
-        flag = True
-        while flag:
-            i += 1
-            buf_start += code_source[i]
-            if len(buf_start) >= length_start:
-                if buf_start == start:
-                    buf = code_source[i]
-                    while buf != ">":
-                        i += 1
-                        buf = code_source[i]
-                    while flag:
-                        i += 1
-                        buf_end += code_source[i]
-                        text += code_source[i]
-                        if len(buf_end) >= length_end:
-                            if buf_end == end:
-                                text = text.replace(end, '')
-                                flag = False
-                            buf_end = buf_end[1: len(end)]
-                buf_start = buf_start[1: len(start)]
-        return text, i
-
-    def _get_represent(self):
-        """ the represent attibute is only made to simplify
-        the __repr__ function. _get_represent is maid to set it"""
-        nb_columns = len(self.tab[0])
-        nb_lines = len(self.tab)
-        text = ""
-        for x in range(nb_columns):
-            text += "-" * 80 + "\n"
-            for y in range(nb_lines):
-                if y == 0:
-                    text += self.tab[y][x] + ": "
-                else:
-                    text += str(y) + ") " + self.tab[y][x] + "   "
-            text += "\n" + "-" * 80 + "\n"
-        return text
-
-    def __repr__(self):
-        """ display only the column heads """
-        return self.represent
